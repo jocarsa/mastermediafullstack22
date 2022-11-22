47af40e1600927b051f0c948d545b4b0c9003566
jocarsa <info@josevicentecarratala.com>
2022-11-22 22:25:56 +0100

    Update 0009-upsatemysql.php

--- Clase 3/02-PHP/0009-upsatemysql.php
+++ Clase 3/02-PHP/0009-upsatemysql.php
@@ -5,3 +5,4 @@ $mysqli = mysqli_connect("localhost", "mastermedia", "mastermedia", "curso");
 $query = "INSERT INTO alumnos VALUES(NULL,'Jorge','Granados','5423532','5345')";
 // Ejecuto la petición contra el servidor
 mysqli_query($mysqli, $query);
+?>
